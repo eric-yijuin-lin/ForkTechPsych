fc2dfcb087a920039b155824cca9bbacf35e28d7
Eric Lin <funnytseric@divc.csie.ndhu.edu.tw>
2020-10-04 11:43:27 +0800

    fitst commit: static index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostContentComponent } from './post-content/post-content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/post-content/post-content.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-content',
+  templateUrl: './post-content.component.html',
+  styleUrls: ['./post-content.component.css', '../../assets/css/style.css'],
+})
+export class PostContentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
